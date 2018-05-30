0297149600f4e92fc625080eee09b223eff564e5
JESUS <JESUS@SPARTAN2392>
2018-05-29 23:27:59 -0500

    Validador busqueda

--- BigStyleBarber/WebContent/js/AdminBarbero.js
+++ BigStyleBarber/WebContent/js/AdminBarbero.js
@@ -1,5 +1,9 @@
-function onClose(){
+function onCloseHoraEntrada(){
 	PF('entradaFinTimeWidget').setTime(PF('entradaIniTimeWidget').getTime());
+} 
+
+function onCloseHoraSalida(){
+	PF('salidaFinTimeWidget').setTime(PF('salidaIniTimeWidget').getTime());
 } 
 
 function tpEntradaFinOnHourShowCallback(hour) {  
@@ -25,6 +29,44 @@ function tpEntradaFinOnMinuteShowCallback(hour, minute) {
   
     var tpStartHour = parseInt(PF('entradaIniTimeWidget').getHours());  
     var tpStartMinute = parseInt(PF('entradaIniTimeWidget').getMinutes());  
+  
+    // Check if proposed hour is after selected start time hour  
+    if (parseInt(hour) > tpStartHour) {  
+        return true;  
+    }  
+  
+    // Check if proposed hour is equal to selected start time hour and minutes is after  
+    if ((parseInt(hour) == tpStartHour) && (parseInt(minute) > tpStartMinute)) {  
+        return true;  
+    }  
+  
+    // if minute did not match, it can not be selected  
+    return false;  
+}  
+
+function tpSalidaFinOnHourShowCallback(hour) {  
+    if (!PrimeFaces.widgets['salidaIniTimeWidget']) {  
+        return false;  
+    }  
+  
+    var tpStartHour = parseInt(PF('salidaIniTimeWidget').getHours());  
+  
+    // Check if proposed hour is after or equal to selected start time hour  
+    if (parseInt(hour) >= tpStartHour) {  
+        return true;  
+    }  
+  
+    // if hour did not match, it can not be selected  
+    return false;  
+}  
+  
+function tpSalidaFinOnMinuteShowCallback(hour, minute) {  
+    if (!PrimeFaces.widgets['salidaIniTimeWidget']) {  
+        return false;  
+    }  
+  
+    var tpStartHour = parseInt(PF('salidaIniTimeWidget').getHours());  
+    var tpStartMinute = parseInt(PF('salidaIniTimeWidget').getMinutes());  
   
     // Check if proposed hour is after selected start time hour  
     if (parseInt(hour) > tpStartHour) {  
